a22251adcd284f8532339bf4fcd04f0623adc5e2
takitopy <65824725+Robertosga@users.noreply.github.com>
2023-02-05 23:33:16 -0300

    Add files via upload

---
+++ banco.py
@@ -0,0 +1,4 @@
+usuario = 'roberto'
+usuario1= 'jesebel'   
+senha= '290804'
+senha1='202020'
---
+++ pagina1.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request
+import pyodbc
+from banco import*
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    variavel='COLABORADORES'
+
+    if request.method == 'GET':
+      return render_template('login.html',variavel1=variavel)
+    else:
+     username = request.form.get('username')
+     password = request.form.get('password')
+
+        # Adicione sua lógica de autenticação aqui
+
+    
+    if username == usuario and password == senha or username == usuario1 and password == senha1 :
+      return render_template('usuario.html')
+    else:
+      return render_template('negado.html')
+
+@app.route('/<string:nome>')
+def error(nome):
+    variavel= f'Página ({nome}) Não Existe'
+    return render_template("error.html",variavel2=variavel)
+
+app.run(debug=True)
